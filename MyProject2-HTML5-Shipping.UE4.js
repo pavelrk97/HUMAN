a2e0127d723f567de1bb298ce1d5d6c4efbb091b
pavelrk97 <79162012+pavelrk97@users.noreply.github.com>
2021-06-27 20:01:26 +0300

    Update MyProject2-HTML5-Shipping.UE4.js

--- MyProject2-HTML5-Shipping.UE4.js
+++ MyProject2-HTML5-Shipping.UE4.js
@@ -1155,7 +1155,7 @@ $(document).ready(function() {
 		// ----------------------------------------
 		// MORE JS
 		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://raw.githubusercontent.com/pavelrk97/HUMAN/main/MyProject2-HTML5-Shipping.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/pavelrk97/HUMAN/c8f7708f0f3f33f2cff6761446db9e02bcf2f230/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('MyProject2-HTML5-Shipping.data')).then(function(dataBlob) {
